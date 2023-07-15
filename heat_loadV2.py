0756bc83a09fd931e90f56d43ea3e04a66d272bb
Jay Chien <zx123c123@gmail.com>
2023-07-15 15:44:00 +0800

    finish the version 2 heat load smulator

--- heat_loadV2.py
+++ heat_loadV2.py
@@ -52,39 +52,36 @@ def calculate_thermalSA(stage_4K, stage_800mK, stage_100mk, stage_10mK, freq):
     #---------------------------------------------------#
     # The original thermal noise power spectrum
     stage_noise=np.vstack([np.zeros(len(freq))]*len(stage))
+
     for i, temp in enumerate(stage):
         stage_noise[i,:] = JN_noise(temp, freq)
 
-
     fig, ax = plt.subplots(figsize=(8,8))
     color_list = ["orange", "blue", "red", "green","black"]
+
     for i, temp in enumerate(stage):
-        ax.plot(f, stage_noise[i],  color = color_list[i], label='{temp}K')
-
-
+        ax.plot(f, stage_noise[i],  color = color_list[i], label=f'{temp}K')
     #---------------------------------------------------#
     # After the attenuator thermal noise power spectrum
+    stage.remove(10e-3) #mixing chamber no attenaution
+    if stage_check["800mK"] == 0:
+        del stage_check["800mK"]
+        stage.remove(0.8)
+        stage_noise_att=np.vstack([np.zeros(len(freq))]*(len(stage)))
+    att_list=np.zeros(len(stage_check))
 
-    # att_list=np.zeros(len(stage_check))
-    
-    # for i in range(len(stage_check)):
-    #     att_list[i] = np.sum(np.array(list(stage_check.values()))[::-1][:len(stage_check)-i])
 
-    # stage_noise_att=np.vstack([np.zeros(len(freq))]*len(stage))
-    # for i, temp in enumerate(stage):
-    #     stage_noise_att[i,:] = JN_noise(temp, freq)*att_list[i]
+    for i in range(len(stage_check)):
+        att_list[i] = np.sum(np.array(list(stage_check.values()))[::-1][:len(stage_check)])
 
-    # for i, temp in enumerate(stage):
-    #     if stage_check["800mK"] != 0:
-    #         ax.plot(f, stage_noise_att[i],  color = color_list[i], label='{temp}K')
-    #     else:
-    #         pass
+    for i, temp in enumerate(stage):
+        if i < len(stage_check):
+            stage_noise_att[i,:] = JN_noise(temp, f)*dB_calculator(att_list[i])
 
-    # ax.plot(f, kelvin_300k_att, "orange", label='300K att',linestyle="dashed")
-    # ax.plot(f, kelvin_4k_att, "blue", label='4K att',linestyle="dashed")
-    # if stage_check["800mK"] != 0:
-    #     ax.plot(f, kelvin_800mk_att, "r", label='800mk att',linestyle="dashed")
-    # ax.plot(f, kelvin_100mk_att, "g", label='100mk att',linestyle="dashed")
+
+    for i, temp in enumerate(stage):
+            ax.plot(f, stage_noise_att[i],  color = color_list[i], label=f'{temp}K', linestyle="dashed")
+
 
     plt.axvline(1e10, linestyle=":")
     ax.legend()
@@ -98,8 +95,10 @@ def calculate_thermalSA(stage_4K, stage_800mK, stage_100mk, stage_10mK, freq):
 
 if __name__=="__main__":
     att=[0, 0, 20, 20, 0 ,20] # attenautor at 50K, 4K, 800mK, 100mK, 10mK
-    f = np.linspace(1e6, 1e13, 2)
+    f = np.linspace(1e6, 1e13, int(1e5))
     # start = time()
-    calculate_thermalSA(20,0,20,20,f)
+    calculate_thermalSA(20,0,20,20,f)  #4K, 800mK, 100mK, 10mK
     # end = time()
     # print(end-start)
+
+ 
