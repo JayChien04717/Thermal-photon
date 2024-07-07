50399be0c556c2a9153d43b5e0324c6bc7d85617
JayChien0417 <139246538+JayChien04717@users.noreply.github.com>
2024-07-07 14:50:12 +0800

    Update heat_loadV2.py

--- heat_loadV2.py
+++ heat_loadV2.py
@@ -14,22 +14,13 @@ Author: Jay Chien
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
-import scipy.constants as sc
+from time import time
+from scipy.constants import h, Boltzmann 
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+
 
 def coaxial_heat(r_out, r_dielectirc,r_center,  T1, T2, length):
-    """_summary_
-
-    Args:
-        r_out (_type_): _description_
-        r_dielectirc (_type_): _description_
-        r_center (_type_): _description_
-        T1 (_type_): _description_
-        T2 (_type_): _description_
-        length (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
     #T1 was upper stage, T2 was lower stage
     #r_out was thermal conductivities of outer conductor
 
@@ -48,13 +39,18 @@ def JN_noise(temp, freq):
         
         
     """
+    # k_b = 1.380649e-23      #Bloteman const
+    # h = 6.626070153e-34     #Plank const
     R = 50                  #Resistor, in microwave line commonly 50ohm
-    const = 4*sc.k*temp*R
-    a = sc.h*freq/(sc.k*temp)
-    b = np.exp(sc.h*freq/(sc.k*temp))-1 #會有inf的問題, np.isinf判斷
-    JN_noise = const*a/b
-    return JN_noise
-    
+    const = 4*Boltzmann*temp*R
+    a = h*freq/(Boltzmann*temp)
+    b = np.exp(h*freq/(Boltzmann*temp))-1 #會有inf的問題, np.isinf判斷
+    s = const*a/b
+    return np.array(s)
+
+def inversJN(value, freq):
+    return (h*freq)/(Boltzmann*np.log(1+(4*50*h*freq/value)))
+
 def db2power(dB):
     """ Convert dB to how power ratial. Convert function is power unit. Also means how 
     much photon(1/A %) can transmit
@@ -67,7 +63,8 @@ def db2power(dB):
     """
 
     A=10**(dB/10) 
-    return A
+    return 1/A
+
 def power2dB(power):
     """ Convert power ratial to dB
 
@@ -92,88 +89,95 @@ def BE_dist(temp, freq):
         Frequency
 
     """
-    n_BE = 1/(np.exp(sc.h*freq/(sc.k*temp))-1)
+    k_b = 1.380649e-23
+    h = 6.626070153e-34
+    n_BE = 1/(np.exp(h*freq/(k_b*temp))-1)
     return n_BE
 
 def noise_photon(freq, att):
     ''' calculation the noise phton at i stage
 
+
     '''
-    stage = [50, 4, 0.8, 0.1, 10e-3]
+    stage = [300, 50, 4, 0.8, 0.1, 10e-3]
 
     A = np.zeros(len(att))
     for i in range(len(att)):
-        A[i] = db2power(att[i])
+        A[i] = 10**(att[i]/10)
     noise = np.zeros(len(stage))
-    n_300 = BE_dist(300, freq)
-    
     for i, element in enumerate(stage):
-        if i == 0:
-            noise[i] = n_300/A[i] + (A[i]-1)/A[i]*BE_dist(element, freq)
-        else:
-            noise[i] = noise[i-1]/A[i] + (A[i]-1)/A[i]*BE_dist(element, freq)
+        n_300 = BE_dist(freq, 300)
+        noise[0]= n_300
+        if i != 0:
+            noise[i] = noise[i-1]/A[i] + (A[i]-1)/A[i]*BE_dist(freq, element)
     return noise
 
-def calculate_thermalSA(stage_4K, stage_800mK, stage_100mk, stage_10mK, freq):
+def calculate_thermalSA(stage_50K, stage_4K, stage_800mK, stage_100mk, stage_20mK, freq, interest_freq):
     """ Calculation the thermal phton power spectrum
     
     
     """
-    stage_check = {"4K":stage_4K, "800mK":stage_800mK, "100mK":stage_100mk, "10mK":stage_10mK}# each stage attenuator
-    stage = [300,4,0.8,0.1,10e-3]
+    stage_check = {
+        "50K":stage_50K,
+        "4K":stage_4K, 
+        "800mK":stage_800mK, 
+        "100mK":stage_100mk, 
+        "20mK":stage_20mK}# each stage attenuator
+    stage = [300,50,4,0.8,0.1,20e-3]
     #---------------------------------------------------#
     # The original thermal noise power spectrum
     stage_noise=np.vstack([np.zeros(len(freq))]*len(stage))
 
+    fig, ax = plt.subplots(figsize=(8,8))
+    
+    color_list = ["b", "g", "c", "m","y", "k"]
+
     for i, temp in enumerate(stage):
         stage_noise[i,:] = JN_noise(temp, freq)
-
-    fig, ax = plt.subplots(figsize=(8,8))
-    color_list = ["orange", "blue", "red", "green","black"]
-
-    for i, temp in enumerate(stage):
         ax.plot(f, stage_noise[i],  color = color_list[i], label=f'{temp}K')
 
     #---------------------------------------------------#
     # After the attenuator thermal noise power spectrum
-    stage.remove(10e-3) #mixing chamber no attenaution
-    if stage_check["800mK"] == 0:
-        #if 800mK stage no attenuator, ignore 800mK
-        del stage_check["800mK"]
-        stage.remove(0.8)
-        stage_noise_att=np.vstack([np.zeros(len(freq))]*(len(stage)))
 
+    stage_noise_att=np.vstack([np.zeros(len(freq))]*(len(stage)))
     att_list=np.zeros(len(stage_check))
 
     for i in range(len(stage_check)):
-        att_list[i] = np.sum(np.array(list(stage_check.values())[::-1])[:len(stage_check)-i])
-
-
+        att_list[i] = sum(list(stage_check.values())[i:])
+    
+    
+    stage.remove(stage[-1])
     for i, temp in enumerate(stage):
         if i < len(stage_check):
-            stage_noise_att[i,:] = JN_noise(temp, f)/db2power(att_list[i])
+            stage_noise_att[i,:] = JN_noise(temp, f)*db2power(att_list[i])
+        ax.plot(freq, stage_noise_att[i],  color = color_list[i], label=f'{temp}K att', linestyle=(0, (3, 5, 1, 5, 1, 5)))
+    total_temperature = sum(stage_noise_att)+stage_noise[-1]
+    ax.plot(freq, total_temperature, label='effect temperature', lw=3, color='r')
 
 
-    for i, temp in enumerate(stage):
-            ax.plot(f, stage_noise_att[i],  color = color_list[i], label=f'{temp}K att', linestyle="dashed")
-    ax.plot(f, sum(stage_noise_att)+stage_noise[-1], label='effect')
-    plt.axvline(1e10, linestyle=":")
+    # ax.plot(freq, JN_noise(inversJN(total_temperature[np.where(freq==6e9)], freq), freq), ls='-.', label='jn')
+    efft_temp = inversJN(total_temperature[np.where(freq==interest_freq)], interest_freq)[0]
+
+
+    ax.plot(freq, JN_noise(efft_temp, freq), ls='-.', lw=3)
+    ax.fill_between(freq, JN_noise(efft_temp-0.02, freq), JN_noise(efft_temp+0.02, freq), alpha=0.3)
+    ax.axvline(interest_freq, linestyle="--")
+    plt.title(f'effect temperature at {interest_freq/1e9:.2f} GHz = {efft_temp:.3f}K')
     ax.legend()
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_xlim(1e6, 1e13)
+    ax.set_ylabel('Power')
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_xlim(1e7, 1e13)
     ax.set_ylim(1e-30, 1e-17)
-    
     plt.show()
-    pass
-    return att_list
+
+
 
 
 
 if __name__=="__main__":
-    f = np.linspace(1e6, 1e13, int(1e5))
-    att = [0,0,0,20,20]
-    calculate_thermalSA(20, 0, 20, 20, f)
+    f = np.arange(1e7, 1e13, 1e7)
+    calculate_thermalSA(0,23,9,0,26,f, 5e9)  #4K, 800mK, 100mK, 10mK
 
 
-    a = noise_photon(6e9, att)
